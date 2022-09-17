fdd380215dff73e70670848562dd9af55df9bc95
Edward Alexis Ortiz <edaortiz@bancolombia.com.co>
2022-09-17 09:08:50 -0500

    Added all the necessary files

---
+++ README.md
@@ -0,0 +1,12 @@
+* This project contains an example of graphQL with apollo server
+* for more information about:
+    *graphQL: https://graphql.org
+    *apollo server: https://www.apollographql.com/docs/apollo-server/
+
+* the project has the follow operations
+    * Query available people
+    * add new person
+
+* Start instructions
+    *npm install
+    *when the dependencies have been installed we type node index.js to start the project
---
+++ index.js
@@ -0,0 +1,80 @@
+
+const { ApolloServer, gql } = require('apollo-server');
+const uuid = require('uuid')
+
+const persons = [
+    {
+        name: "Midu",
+        phone: "034-1234567",
+        street: "Calle frontend",
+        city: "Barcelona",
+        id: "09393993939-939393-93939-9393939393"
+    }, {
+        name: "Yourself",
+        phone: "034-1234567",
+        street: "Avenida fullstack",
+        city: "Mataro",
+        id: "123333993939-939393-93939-9393939393"
+    }, {
+        name: "Itzi",
+        street: "Pasaje testing",
+        city: "Ibiza",
+        id: "4443993939-939393-93939-9393939393"
+    }
+]
+
+/**Definiciones de los datos */
+const typeDefinitions = gql`
+type Person{
+    name: String!
+    phone: String
+    street: String!
+    city: String!
+    id: ID!
+}
+
+type Query{
+    personCount: Int!
+    allPersons: [Person]!
+    findPerson(name: String!): Person
+    
+}
+
+type Mutation{
+    addPerson(
+        name: String!
+        phone: String
+        street: String!
+        city: String!
+    ): Person
+}
+`
+
+const resolver = {
+    Query: {
+        personCount: () => persons.length,
+        allPersons: () => persons,
+        findPerson: (root, args) => {
+            const { name } = args
+            return persons.find(person => person.name === name)
+        }
+    },
+    Mutation: {
+        addPerson: (root, args)=>{
+            const person = {...args, id: uuid() }
+            persons.push(person)
+            return person
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs: typeDefinitions,
+    resolvers: resolver
+})
+
+server.listen().then(({ url }) => {
+    console.log(`Server ready at ${url}`)
+});
+
+
